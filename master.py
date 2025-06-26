17c549a55e0e0dedd5ed2e3bbb56e2c21fb2a6f8
Sathyanarayana S <sathyasn15@gmail.com>
2025-06-26 08:24:41 +0530

    Time required to become a master.

---
+++ master.py
@@ -0,0 +1,11 @@
+total = 10000
+#effort = 3
+effort = int(input("what is your daily effort? "))
+
+days = total // effort
+rem_hrs = total % effort
+months = days // 30
+days = days % 30
+years = months // 12
+months = months % 12
+print(years, "yrs", months, "months", days, "days", rem_hrs, "hrs")
