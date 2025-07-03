4d4a715d15517f2777338b1d9f915f2f3b87cdc5
Sathyanarayana S <sathyasn15@gmail.com>
2025-07-03 08:39:32 +0530

    multiplication table of user desired number.
    multiplication table of user desired number.

---
+++ any_num_table.py
@@ -0,0 +1,7 @@
+n = 15
+Header = ("Multiplication Table of:",n)
+print(Header,n*1,n*2,n*3,n*4,n*5,n*6,n*7,n*8,n*9,n*10)
+
+#n = input("please enter the number you want:")
+#header = "multiplication table of" 
+#print(header, n,"x1 =",n*1,n,"x2 =",n*2,n,"x3 =",n*3,n,"x4 =",n*4,n,"x5 =",n*5,n,"x6 =",n*6,n,"x7 =",n*7,n,"x8 =",n*8,n,"x9 =",n*9,n,"x10 =",n*10)
